bed491e67ae5400ded678d4927ab1353e18486c6
mathieu junes <matyjunes@gmail.com>
2018-02-10 16:49:52 +0100

    remove space in files

--- templates/footer.php
+++ templates/footer.php
@@ -1,30 +1,5 @@
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 	<script  src="http://code.jquery.com/jquery-3.3.1.js">	</script>
 	<script type="text/javascript" src="asset/js/jquery-3.2.1.min.js"></script>
-
-
 	<script type="text/javascript" src="asset/js/script.js"></script>
-
-	
 </body>
 </html>
