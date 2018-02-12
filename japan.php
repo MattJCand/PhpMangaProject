4810a332856a292474df98ddf06880eba9a9d5b8
mathieu junes <matyjunes@gmail.com>
2018-02-12 11:57:43 +0100

    AddBlocksCours

--- japan.php
+++ japan.php
@@ -1,6 +1,30 @@
 
 <?php include "templates/header.php"; ?>
 
-<h1>Cours de Japonais</h1>
+<section>
+  <div class="horaireCours">
+    <h1 class="align">Japonais</h1>
+  </div>
+</section>
+
+<section class="descriptionCours">
+  <div class="contentCours">
+    <h4 class="align">Objectif du cours</h4>
+    <p>Lorem ipsum dolor </p>
+  </div>
+</section>
+
+<section class="horaireCours">
+  <div>
+    <h4 class="align">Horaires</h4>
+  </div>
+</section>
+
+<section class="SliderCours">
+  <div>
+    <h4 class="align">Slider</h4>
+  </div>
+</section>
+
 
 <?php include "templates/footer.php"; ?>
