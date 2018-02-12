9161b8edc5b3e1d5c79e5fc00afe9dbfabf89829
mathieu junes <matyjunes@gmail.com>
2018-02-12 12:07:22 +0100

    add block partners

--- partner.php
+++ partner.php
@@ -3,4 +3,34 @@
 
 <h1>Nos partenaires</h1>
 
+<section class="sectionPartner">
+  <div class="containerPartner">
+    <div class="imagePartner">
+      <img src="#">
+    </div>
+    <div class="partnerDescription">
+      <p>Title</p>
+      <p>Description</p>
+    </div>
+  </div>
+  <div class="containerPartner">
+    <div class="imagePartner">
+      <img src="#">
+    </div>
+    <div class="partnerDescription">
+      <p>Title</p>
+      <p>Description</p>
+    </div>
+  </div>
+  <div class="containerPartner">
+    <div class="imagePartner">
+      <img src="#">
+    </div>
+    <div class="partnerDescription">
+      <p>Title</p>
+      <p>Description</p>
+    </div>
+  </div>
+</section>
+
 <?php include "templates/footer.php"; ?>
