5db02ed98f15e2bc9573d1a03ee7d7ac7d00cc19
Jeremie <feinglong@gmail.com>
2018-02-12 13:33:30 +0100

    change branch

--- index.php
+++ index.php
@@ -1,7 +1,63 @@
 
 <?php include "templates/header.php"; ?>
 
-<section id="sliderFirst"></section>
+<section id="caroussel-banniere">
+    
+        <div class="fade">
+
+          <div class="bloc-banniere banniere-japonnais">
+            <div class="container">
+              <div class="bloc-titre-banniere">
+                <h1 class="majuscule">Japonnais</h1>
+              </div>
+
+              <div class="bloc-texte-banniere">
+                <p>
+                  Découverte des syllabaires (hiragana et katakana) et des premiers kanji de niveau élémentaire, apprendre à lire, à écrire et à comprendre des phrases relativement simples.
+                </p>
+              </div>
+            </div>
+          </div>
+
+
+          <div class="bloc-banniere banniere-manga">
+            <div class="container">
+              <div class="bloc-titre-banniere">
+                <h1 class="majuscule">manga</h1>
+              </div>
+
+              <div class="bloc-texte-banniere">
+                <p>
+                  Découverte des syllabaires (hiragana et katakana) et des premiers kanji de niveau élémentaire, apprendre à lire, à écrire et à comprendre des phrases relativement simples.
+                </p>
+              </div>
+            </div>
+          </div>
+
+
+          <div class="bloc-banniere banniere-dessin">
+            <div class="container">
+              <div class="bloc-titre-banniere">
+                <h1 class="majuscule">dessin</h1>
+              </div>
+
+              <div class="bloc-texte-banniere">
+                <p>
+                  Découverte des syllabaires (hiragana et katakana) et des premiers kanji de niveau élémentaire, apprendre à lire, à écrire et à comprendre des phrases relativement simples.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          
+            
+        </div>
+  
+</section>
+
+
+
+
 
 <section id="sectionCours">
   <div class="container">
@@ -14,15 +70,19 @@
 <section id="carteMember">
 </section>
 
-<section id="social">
+<!-- <section id="social">
   <h4>Actualités</h4>
   <div id="actu">
     <div id="fb-root"></div>
     <div class="fb-page" data-href="https://www.facebook.com/planete.manga.muret/" data-tabs="timeline" data-width="500" data-height="400" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false"><blockquote cite="https://www.facebook.com/planete.manga.muret/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/planete.manga.muret/">Planète Manga</a></blockquote></div>
   </div>
+
+</section> -->
+
+<!-- <section>
   <h4>Partenaires</h4>
   <div id="partner"></div>
 </section>
-
+ -->
 <?php include "templates/footer.php"; ?>
 
--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,53 @@
+	<!-- <footer>
+		<div class="container">
+
+			<div>
+				<div class="footer-block-ecole">
+					<p class="bold">Ecole</p>
+					<ul>
+					
+						<li><a href="">Galerie</a></li>
+						<li><a href="">A propos</a></li>
+						<li><a href="">Nos partenaire</a></li>
+					</ul>
+					
+					
+				</div>
+				<div></div>
+				<div></div>
+				<div></div>
+			</div>
+
+			<div>
+				
+			</div>
+			
+			<div>
+				
+			</div>
+
+		</div>
+
+	</footer>
+
+ -->
+
+
+
+	
+	
 	<script  src="http://code.jquery.com/jquery-3.3.1.js">	</script>
-	<script type="text/javascript" src="asset/js/jquery-3.2.1.min.js"></script>
+
+	<script type="text/javascript" src="asset/slick/slick.min.js"></script>
+	
 	<script type="text/javascript" src="asset/js/script.js"></script>
+
+	<script type="text/javascript" src="asset/js/caroussel-script.js"></script>
+			
+	
+	<!-- ***** -->
+	
+	
+
 </body>
 </html>
